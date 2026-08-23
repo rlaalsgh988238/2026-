c35b02e8a711fe9d74f57656a6950e3cfa3afd88
KimMinHo <kmh030877@gmail.com>
2026-08-23 09:14:20 +0900

    fix: Local Mapper 내부 ToiletDataMapper #18

--- local/src/main/java/com/braveberry/local/model/ToiletDataLocalModel.kt
+++ local/src/main/java/com/braveberry/local/model/ToiletDataLocalModel.kt
@@ -73,3 +73,33 @@ internal data class ToiletDataLocalModel(
             updateDate
         )
 }
+
+internal fun ToiletDataModel.toLocal(): ToiletDataLocalModel = ToiletDataLocalModel(
+    id = id,
+    toiletName = toiletName,
+    roadAddress = roadAddress,
+    lotAddress = lotAddress,
+    isUnisex = isUnisex,
+    maleToiletBowlCount = maleToiletBowlCount,
+    maleUrinalCount = maleUrinalCount,
+    maleDisabledToiletCount = maleDisabledToiletCount,
+    maleDisabledUrinalCount = maleDisabledUrinalCount,
+    maleChildToiletCount = maleChildToiletCount,
+    maleChildUrinalCount = maleChildUrinalCount,
+    femaleToiletBowlCount = femaleToiletBowlCount,
+    femaleDisabledToiletCount = femaleDisabledToiletCount,
+    femaleChildToiletCount = femaleChildToiletCount,
+    managingAgency = managingAgency,
+    phoneNumber = phoneNumber,
+    openTime = openTime,
+    latitude = latitude,
+    longitude = longitude,
+    emergencyBellExists = emergencyBellExists,
+    cctvExists = cctvExists,
+    diaperChangingStationExists = diaperChangingStationExists,
+    updateDate = updateDate
+)
+
+internal fun List<ToiletDataModel>.toLocal(): List<ToiletDataLocalModel> {
+    return map { it.toLocal() }
+}

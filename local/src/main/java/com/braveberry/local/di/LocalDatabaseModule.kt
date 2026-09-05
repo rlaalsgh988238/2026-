928162d7e852afeea0ffb345adcc1c0b64c2bdc2
KimMinHo <kmh030877@gmail.com>
2026-09-05 14:44:45 +0900

    fix: 모듈 통합

--- local/src/main/java/com/braveberry/local/di/LocalDatabaseModule.kt
+++ local/src/main/java/com/braveberry/local/di/LocalDatabaseModule.kt
@@ -1,24 +1,22 @@
 package com.braveberry.local.di
 
 import android.content.Context
-import androidx.room.Room
+import com.braveberry.local.roomDB.AppDatabase
+import com.braveberry.local.roomDB.DatabaseRegistrationManager
+import com.braveberry.local.roomDB.dao.CourseDao
+import com.braveberry.local.roomDB.dao.RegionDataDao
+import com.braveberry.local.roomDB.dao.ToiletDataDao
+import com.braveberry.local.util.Log
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import com.braveberry.local.roomDB.AppDatabase
-import com.braveberry.local.roomDB.DatabaseRegistrationManager
-import com.braveberry.local.roomDB.RoomConstant
-import com.braveberry.local.roomDB.dao.CourseDao
-import com.braveberry.local.roomDB.dao.RegionDataDao
-import com.braveberry.local.roomDB.dao.ToiletDataDao
-import com.braveberry.local.util.Log
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-internal object LocalRoomModule {
+internal object LocalDatabaseModule {
 
     @Provides
     @Singleton
@@ -28,16 +26,10 @@ internal object LocalRoomModule {
     ): AppDatabase {
         Log.d("TOUR_DATA_DEBUG", "1. buildDatabase called")
         val db = AppDatabase.buildDatabase(context, registrationManager)
-
-        // 🌟 중요: DB를 강제로 오픈시킵니다.
-        // 이 코드가 실행되는 순간 Room의 Callback(onCreate/onOpen)이 트리거됩니다.
-        db.openHelper.writableDatabase
-
+        db.openHelper.writableDatabase // DB 강제 오픈
         return db
     }
 
-
-    // 모든 DAO 제공을 여기서 관리
     @Provides
     @Singleton
     fun provideToiletDao(database: AppDatabase): ToiletDataDao = database.toiletDao()
@@ -45,5 +37,8 @@ internal object LocalRoomModule {
     @Provides
     @Singleton
     fun provideRegionDataDao(database: AppDatabase): RegionDataDao = database.regionDao()
-}
 
+    @Provides
+    @Singleton
+    fun provideCourseDao(database: AppDatabase): CourseDao = database.courseDao()
+}
